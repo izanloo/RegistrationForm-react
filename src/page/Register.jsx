5405efa2cd25fbf9f5a11b31ea9268a96362ea37
izanloo <razieh.izanloo755@gmail.com>
2023-01-01 12:38:18 -0800

    feat:image only jpg files

--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
 import imgRegister from '../assest/images/img-page-resister.webp'
 import imgDefault from '../assest/images/profile-gray.webp'
-import {LabelError,LabelInput} from "./components/Labels";
+import { LabelError, LabelInput } from "./components/Labels";
 
 
 export default function Register() {
@@ -17,6 +17,8 @@ export default function Register() {
         if (event.target.files && event.target.files[0]) {
             let reader = new FileReader();
             let file = event.target.files[0];
+            console.log(event.target.files[0])
+
             reader.onloadend = () => {
                 setData({
                     ...mydata,
@@ -40,29 +42,29 @@ export default function Register() {
                 <form onSubmit={handleSubmit(onSubmit)} className="w-full rounded-tr-lg rounded-br-lg shadow">
                     <div className="py-4 px-8 ">
                         <div className="flex flex-col items-center mb-3">
-                            <label htmlFor="file" className="block w-36 h-36 border border-black bg-cover border-dashed rounded-full text-center " style={{ backgroundImage: mydata !='' ? `url(${mydata.imagePreview})` : `url(${imgDefault})` }}></label>
-                            <input type="file" id="file" className="hidden" name="imgProfile" {...imgProfile} onChange={e => { imgProfile.onChange(e); handleImageUpload(e) }} />
+                            <label htmlFor="file" className="block w-36 h-36 border border-black bg-cover border-dashed rounded-full text-center " style={{ backgroundImage: mydata != '' ? `url(${mydata.imagePreview})` : `url(${imgDefault})` }}></label>
+                            <input type="file" accept=".jpg" id="file" className="hidden" name="imgProfile" {...imgProfile} onChange={e => { imgProfile.onChange(e); handleImageUpload(e) }} />
                             <ErrorMessage errors={errors} name="imgProfile" render={({ message }) => <LabelError msg={message} />} />
                         </div>
                         <div className="flex mb-4">
                             <div className="w-1/2 mr-1">
-                                <LabelInput name="First Name" htmlFor="firstName"/>
+                                <LabelInput name="First Name" htmlFor="firstName" />
                                 <input className="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="firstName" type="text" placeholder="first name" {...register("firstName", { required: "firstName is required" })} />
                                 <ErrorMessage errors={errors} name="firstName" render={({ message }) => <LabelError msg={message} />} />
                             </div>
                             <div className="w-1/2 ml-1">
-                                <LabelInput htmlFor="lastName" name="Last Name"/>
+                                <LabelInput htmlFor="lastName" name="Last Name" />
                                 <input className="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="lastName" type="text" placeholder="last name" {...register("lastName", { required: "lastName is required" })} />
                                 <ErrorMessage errors={errors} name="lastName" render={({ message }) => <LabelError msg={message} />} />
                             </div>
                         </div>
                         <div className="mb-4">
-                            <LabelInput htmlFor="dateBirth" name="Date Birth"/>
+                            <LabelInput htmlFor="dateBirth" name="Date Birth" />
                             <input className="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="dateBirth" type="date"  {...register("dateBirth", { required: "lastName is required" })} />
                             <ErrorMessage errors={errors} name="dateBirth" render={({ message }) => <LabelError msg={message} />} />
                         </div>
                         <div className="mb-4">
-                            <LabelInput htmlFor="Age" name="Age"/>
+                            <LabelInput htmlFor="Age" name="Age" />
                             <input className="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="Age" type="number" placeholder="Age" {...register("Age", { required: "Age is required" })} />
                             <ErrorMessage errors={errors} name="Age" render={({ message }) => <LabelError msg={message} />} />
                         </div>
