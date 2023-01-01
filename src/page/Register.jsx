007bf226375c9830cc6c51265b0a1e7b517618d3
izanloo <razieh.izanloo755@gmail.com>
2023-01-01 05:06:11 -0800

    add hook useform and add react-icons

--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -1,7 +1,25 @@
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { BiErrorCircle } from "react-icons/bi";
+
 export default function Register() {
+    // useform for get value inputs and handle error validation
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     return (
-    <>
-        page register
-    </>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label className="block mb-2" htmlFor="username">name</label>
+            <input className='w-full p-2  border border-gray-400 rounded-lg bg-gray-100'
+                {...register("username", {
+                    required: "نام کابری را وارد کنید"
+                })}
+            />
+            <ErrorMessage errors={errors} name="username" render={({ message }) => <p className="text-red-700 pt-2 flex items-center"><BiErrorCircle />{message}</p>} />
+            <input type="submit" className="w-full text-white bg-[#8c2973] font-bold py-2 px-4 my-6 rounded-lg" value="ورود" />
+
+        </form>
     )
 }
